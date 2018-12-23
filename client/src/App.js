3b796ca64a1299a802cb27aa0106fcbcd7d20f42
Mahmoud-Zaid94 <momozd13@gmail.com>
2018-12-23 19:57:18 +0200

    (feat) sign in component in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-
+import SignUp from './components/auth/Signup'
 class App extends Component {
  
   render() {
     return (
-      <BrowserRouter>
-        <div className="App container">
-          <h1 className="center text-grey">Welcome with Doctor Tech</h1>
-          <h3 className="center text-grey"> My Team I wish you all the best.</h3>
-          <Navbar />
-          <Route />
-          <Route />
-          <Route />
-          <Route />
-        </div>
-      </BrowserRouter>
+     
+          <SignUp />
+         
     );
   }
 }
