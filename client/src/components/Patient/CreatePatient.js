dbcd736456ae8e0a3a778c6957d40ad922672613
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-12-23 21:16:32 +0300

    (fix) add the patient personal information

--- client/src/components/Patient/CreatePatient.js
+++ client/src/components/Patient/CreatePatient.js
@@ -8,23 +8,41 @@ class CreatePatient extends Component {
 
   state = {
     firstName:'',
+    MiddleName:'',
+    lastName:'',
+    age:'',
+    Phone:'',
+    insurance:'',
+    email:'',
+    location:'',
+    selectedOption :'Male',
+    checked:false
 
-    name: '',
-    index: ''
   }
 
 //Note: handle submit information
   handleSubmit= (e) => {
     e.preventDefault()
+    console.log(this.state)
   }
 //Note: handle on change information
   handleChange=(e)=>{
-    this.setState({
+
+ this.setState({
         [e.target.id]:e.target.value
     })
   }
 
+  //Note: handle on change option 
+  handleOptionChange=(e)=>{
+    console.log(e.target.value)
+    this.setState({
+      selectedOption: e.target.value
+    });
+  }
+
   render() {
+   
     return (
       <div>
         <div className="container">
@@ -35,17 +53,17 @@ class CreatePatient extends Component {
               <div className="col s12">
                 <div className="input-field col s3">
                   <i className="material-icons prefix">account_circle</i>
-                  <input id="firstName" type="text" className="validate" />
+                  <input onChange={this.handleChange} id="firstName" type="text" className="validate" />
                   <label htmlFor="firstName">First Name</label>
                 </div>
                 <div className="input-field col s3">
                   <i className="material-icons prefix">account_circle</i>
-                  <input id="MiddleName" type="text" className="validate" />
+                  <input onChange={this.handleChange} id="MiddleName" type="text" className="validate" />
                   <label htmlFor="MiddleName">Middle Name</label>
                 </div>
                 <div className="input-field col s3">
                   <i className="material-icons prefix">account_circle</i>
-                  <input id="lastName" type="text" className="validate" />
+                  <input onChange={this.handleChange} id="lastName" type="text" className="validate" />
                   <label htmlFor="lastName">Last Name</label>
                 </div>
               </div>
@@ -53,17 +71,17 @@ class CreatePatient extends Component {
               <div className="col s12 ">
                 <div className="input-field col s3">
                   <i className="material-icons prefix">assignment_ind</i>
-                  <input id="age" type="number" className="validate " />
+                  <input onChange={this.handleChange} value={this.state.age} id="age" type="number" className="validate " />
                   <label htmlFor="age">Age</label>
                 </div>
                 <div className="input-field col s3">
                   <i className="material-icons prefix">phone</i>
-                  <input id="Phone" type="tel" className="validate" />
+                  <input onChange={this.handleChange} value={this.state.Phone} id="Phone" type="tel" className="validate" />
                   <label htmlFor="Phone">Phone</label>
                 </div>
                 <div className="input-field col s3">
                   <i className="material-icons prefix">assessment</i>
-                  <input id="insurance" type="number" className="validate" />
+                  <input onChange={this.handleChange} value={this.state.insurance} id="insurance" type="number" className="validate" />
                   <label htmlFor="insurance">insurance Number</label>
                 </div>
               </div>
@@ -71,7 +89,7 @@ class CreatePatient extends Component {
               <div className="col s12">
                 <div className="input-field col s9">
                   <i className="material-icons prefix">email</i>
-                  <input id="email" type="email" className="validate" />
+                  <input onChange={this.handleChange} value={this.state.email} id="email" type="email" className="validate" />
                   <label htmlFor="email">Email</label>
                 </div>
               </div>
@@ -79,7 +97,7 @@ class CreatePatient extends Component {
               <div className="col s12">
                 <div className="input-field col s9">
                   <i className="material-icons prefix">location_on</i>
-                  <input id="location" type="text" className="validate" />
+                  <input onChange={this.handleChange} value={this.state.location} id="location" type="text" className="validate" />
                   <label htmlFor="location">location</label>
                 </div>
               </div>
@@ -90,20 +108,20 @@ class CreatePatient extends Component {
                 </div>
                 <div className="input-field col s1">
                   <label>
-                    <input name="gender" type="radio" checked />
+                    <input onChange={this.handleOptionChange} value="Male" name="gender" type="radio" checked={this.state.selectedOption=='Male' } />
                     <span>Male</span>
                   </label>
                 </div>
                 <div className="input-field col s2">
                   <label>
-                    <input name="gender" type="radio" />
+                    <input onChange={this.handleOptionChange} value="FeMale" name="gender" type="radio" checked={this.state.selectedOption=='FeMale' }/>
                     <span>FeMale</span>
                   </label>
                 </div>
                 <div className="input-field col s2"></div>
                 <div className="input-field col s2">
                   <label>
-                    <input type="checkbox" />
+                    <input onChange={()=>{this.setState({checked:!this.state.checked}) }} type="checkbox" />
                     <span>Married</span>
                   </label>
 
@@ -113,7 +131,7 @@ class CreatePatient extends Component {
               <div className="col s12">
                 <div className="input-field col s12">
                   <br /><br /><br /><br /><br />
-                  <Button className="CreatePatientSubmit" color="secondary" size="lg" block>Block level button</Button>
+                  <Button className="CreatePatientSubmit" color="secondary" size="lg" block>Register</Button>
                 </div>
               </div>
 
