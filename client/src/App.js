4dd148b076d2b5fcb77d790a4a741f6d6ca4771b
mhd-rawashdah <mhd.rawashdah@gmail.com>
2018-12-24 03:27:47 +0200

    (feat) create router and NavBar and dashboard structure in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import SignIn from './components/auth/SignIn'
-import SignUp from './components/auth/Signup'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './components/layout/Header.js'
+import Dashboard from './components/dashboard/Dashboard.js';
+import SignIn from './components/auth/SignIn.js';
+import Signup from './components/auth/Signup.js';
+
 class App extends Component {
- 
+  state = {
+    isLogin: true
+  }
   render() {
-    return {
-       <div>
-      </div>
+    return (
+      <BrowserRouter>
+        <div>
+          {this.state.isLogin ? <Header /> : ''}
+          <Switch>
+            {/* Router all the component  ToDO add the component */}
+            <Route exact path="/" />
+            <Route path="/signin" />
+            <Route path="/signup"  />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
