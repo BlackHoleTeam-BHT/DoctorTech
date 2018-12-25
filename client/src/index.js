761e9e435a66d148dce44c9e0815b78abd3b1aa3
walid-hajhussien <eng.waleed_com@yahoo.com>
2018-12-25 12:50:17 +0300

    (feat) add ChifComplaint component

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CreatePatient from './components/Patient/CreatePatient2'
 import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import rootReducer from './store/reducer/rootReducer'
 import thunk from 'redux-thunk'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ChiefComplaint from './components/Patient/Profile/patientComponent/chiefComplent'
 
 // Note:create the store and add the thunk Middleware
 const store=createStore(rootReducer,applyMiddleware(thunk))
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
-
+ReactDOM.render(<Provider store={store}><ChiefComplaint/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
