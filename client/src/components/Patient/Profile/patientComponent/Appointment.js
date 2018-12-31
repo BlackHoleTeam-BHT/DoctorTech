5ef4053f9f3bf0a4c974a92b4f75cc509a834fa4
Mahmoud-Zaid94 <momozd13@gmail.com>
2018-12-31 11:24:21 +0200

    (feat) compleate design for Appointment component

--- client/src/components/Patient/Profile/patientComponent/Appointment.js
+++ client/src/components/Patient/Profile/patientComponent/Appointment.js
@@ -50,8 +50,11 @@ const styles = theme => ({
 class Appointment extends React.Component {
   state = {
     open: false,
+    date: "",
+    notes: "",
   };
 
+  // take value from inputs
   handleChange2 = name => event => {
     this.setState({
       [name]: event.target.value,
@@ -63,7 +66,7 @@ class Appointment extends React.Component {
     });
   };
 
- handleClickOpen = () => {
+  handleClickOpen = () => {
     this.setState({ open: true });
   };
 
@@ -71,14 +74,21 @@ class Appointment extends React.Component {
     this.setState({ open: false });
   };
 
-
+  handelSubmit = () => {
+    var obj = {
+      date: this.state.date,
+      notes: this.state.notes,
+      id_Patients: this.props.patient.id,
+      id_Doctors: this.props.doctors.id
+    }
+    this.props.AddChiefComplaint(obj)
+  }
 
   render() {
- 
     const { classes } = this.props;
     console.log("mmmmmmmmmmmmmm", this.props)
     return (
-       <div>
+      <div>
         <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
           Open form dialog
         </Button>
@@ -87,43 +97,48 @@ class Appointment extends React.Component {
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">New Pointment</DialogTitle>
+          <DialogTitle id="form-dialog-title">New Appointment</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              Please enter new pointment time and date .
+              Please enter new Appointment time and date .
             </DialogContentText>
-           
+
             <Grid container md={12} item ustify="space-around"  >
-            
-            <TextField
-        id="datetime-local"
-        label="Add Appointment"
-        type="datetime-local"
-        defaultValue="2019-03-01T10:30"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-      </Grid>
-    
-      <Grid md={12} item>
-   <TextField
-          id="outlined-multiline-static"
-          label="Add DEscription"
-          multiline
-          rows="8"
-          className={classes.textField}
-          margin="normal"
-          variant="outlined"
-          
-        />
-        </Grid>
-        
-        
+
+              <TextField
+
+                id="datetime-local"
+                label="Add Appointment"
+                type="datetime-local"
+                value={this.state.date}
+                defaultValue="2019-03-01T10:30"
+                onChange={this.handleChange2('date')}
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+
+            <Grid md={12} item>
+              <TextField
+                id="outlined-multiline-static"
+                label="Add Notes"
+                multiline
+                rows="8"
+                className={classes.textField}
+                margin="normal"
+                variant="outlined"
+                value={this.state.notes}
+                onChange={this.handleChange2('notes')}
+              />
+            </Grid>
+
+
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.handleClose} color="primary" variant="contained">
+            {/* <Button onClick={(event)=>{this.handelSubmit();this.handleClose()}} color="primary" variant="contained"> */}
+            <Button onClick={this.handelSubmit} color="primary" variant="contained">
               Add
             </Button>
             <Button onClick={this.handleClose} color="primary" variant="contained">
@@ -140,6 +155,22 @@ Appointment.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
+//Note:add the redux state to the props
+const mapStateToProps = (state) => {
+  return {
+    patient: state.patient,
+    doctors: state.users
+  }
+}
+
+// Note: add the action to the props
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     AddAppointment: (data) => dispatch(AddAppointment(data)),
+
+//   }
+// }
 
 
-export default compose(withStyles(styles), connect())(Appointment);
+// export default compose(withStyles(styles), connect(mapStateToProps,mapDispatchToProps))(Appointment);
+export default compose(withStyles(styles), connect(mapStateToProps))(Appointment);
