66674831131a6351b9958a1c9a6803990071f918
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-01-09 17:30:35 +0300

    refactor index js file

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import DrProfile from './components/doctorProfile/DrProfile'
 // Note:create the store and add the thunk Middleware
 const store=createStore(rootReducer,applyMiddleware(thunk))
 
-ReactDOM.render(<Provider store={store}><MedicalNews/></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
