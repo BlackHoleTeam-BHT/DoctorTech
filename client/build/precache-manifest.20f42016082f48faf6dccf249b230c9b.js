31791ecbac3bd9d3ad175c76de50954c3b2ffde9
mhd-rawashdah <mhd.rawashdah@gmail.com>
2019-01-10 01:02:43 +0200

    make npm build1

---
+++ client/build/precache-manifest.20f42016082f48faf6dccf249b230c9b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "28a9d828a03bdf163425",
+    "url": "/static/css/main.6d828f35.chunk.css"
+  },
+  {
+    "revision": "28a9d828a03bdf163425",
+    "url": "/static/js/main.28a9d828.chunk.js"
+  },
+  {
+    "revision": "b8f8ada2a979ab8d336c",
+    "url": "/static/css/1.b96b172c.chunk.css"
+  },
+  {
+    "revision": "b8f8ada2a979ab8d336c",
+    "url": "/static/js/1.b8f8ada2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "47294677aad682394d12d28242abec4b",
+    "url": "/static/media/head2.47294677.jpg"
+  },
+  {
+    "revision": "3a5ab37b4c90972c3c4b97cfc4775b6e",
+    "url": "/static/media/body2.3a5ab37b.jpg"
+  },
+  {
+    "revision": "27948eb07f5ad98df8687fb1e7cd2760",
+    "url": "/static/media/legs2.27948eb0.jpg"
+  },
+  {
+    "revision": "fc75d06a589ee9ee44fb7e7f76f005a6",
+    "url": "/static/media/calculater.fc75d06a.gif"
+  },
+  {
+    "revision": "d878c5c2740078eab178f8debc414391",
+    "url": "/static/media/head.d878c5c2.gif"
+  },
+  {
+    "revision": "7bc0f077f08b7294f0717f56cf01dbaf",
+    "url": "/static/media/body.7bc0f077.gif"
+  },
+  {
+    "revision": "4f3d4e1c22c71ed9f2fd901edab8c597",
+    "url": "/static/media/legs.4f3d4e1c.gif"
+  },
+  {
+    "revision": "51cfa45f02e233ac2c5b2eee0235cdc6",
+    "url": "/static/media/doctor2.51cfa45f.jpg"
+  },
+  {
+    "revision": "a31ae5c9d58bd72cb8b510c90cd7f261",
+    "url": "/static/media/doctor.a31ae5c9.jpg"
+  },
+  {
+    "revision": "8b50a7f34ba0664a13cdb916f3b746fe",
+    "url": "/static/media/logo.8b50a7f3.png"
+  },
+  {
+    "revision": "dd5ba8ddd7400cdb54244f83955732a3",
+    "url": "/static/media/d3.dd5ba8dd.jpg"
+  },
+  {
+    "revision": "a3603039da8383798f7eb951c894dc37",
+    "url": "/static/media/drSimson.a3603039.jpg"
+  },
+  {
+    "revision": "1082ae219d4c0fd1c602b823bf6bfafe",
+    "url": "/static/media/d4.1082ae21.jpg"
+  },
+  {
+    "revision": "7c0f67ae61c98d48ee278fbb3a59371a",
+    "url": "/index.html"
+  }
+];
